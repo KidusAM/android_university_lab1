38e0f5c32a99db1f848c7eebd29ee3a2a7659111
Kidus <km3533@columbia.edu>
2022-02-19 14:37:34 -0500

    Added my API key

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.kt
@@ -20,7 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory
  * after copy and paste the API key under your Account -> Apps -> <Your App> -> API Keys
 </Your> */
 // TODO: Replace the below API key with your own generated key
-private const val API_KEY = "<YOUR-API-KEY-GOES-HERE>"
+private const val API_KEY = "SoSL9P7GrH2qXv5WstsHtmnGJlkAfKHV"
 class NYTimesApiClient {
     private val nyTimesService: NYTimesService
 
